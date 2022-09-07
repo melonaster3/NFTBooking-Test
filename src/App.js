f1e11f5f1442d6a913f9e223d0a58068107017b6
Sang <melonaster@gmail.com>
2022-09-07 19:37:48 +0900

    Finalized for NFTBooking

--- src/App.js
+++ src/App.js
@@ -7,11 +7,18 @@ import {
   useMintNFT,
   useEditionDrop,
   useClaimNFT,
+  useNFTs,
+  useNFTBalance,
+  useNFT,
+  useTotalCount,
+  useOwnedNFTs
 } from "@thirdweb-dev/react";
 
+import "./App.css";
+
 import standard from "./NFTBooking.jpg";
-import silver from "./2.jpg";
-import gold from "./3.jpg";
+import gold from "./2.jpg";
+import silver from "./3.jpg";
 
 export default function Home() {
   const connectWithMetamask = useMetamask();
@@ -19,70 +26,58 @@ export default function Home() {
   const address = useAddress();
   const contractAddress = "0x050E86c51107dc696d2d73052BCE390C8034f04C";
   const editionDrop = useEditionDrop(contractAddress);
+const { data: nfts, isLoadingNFT, errorNFT } = useNFTs(editionDrop);
+const { mutate: claimNft, isLoading, error } = useClaimNFT(editionDrop);
+const { data: ownedNFTs, isLoadingOwner, errorOwner } = useOwnedNFTs(editionDrop,address);
+const { data: totalCount, isLoadingTotalCount, errorTotalCount } = useTotalCount(editionDrop);
 
-  const { mutate: claimNft, isLoading, error } = useClaimNFT(editionDrop);
+
+
+if (isLoading || !nfts ) {
+  return <div>LOADING</div>;
+}
+
+console.log(nfts);
+console.log(nfts[0].metadata.id.toNumber());
+console.log(totalCount.toNumber())
+console.log(ownedNFTs)
+
+const nftList = nfts.map((nft) => (
+  <div>
+  <img
+  src = {nft.metadata.image}
+  width = "250px"
+  length = "250px"
+  />
+   <button
+     disabled={isLoading}
+     onClick={() => claimNft({ to: address, quantity: 1, tokenId: nft.metadata.id.toNumber()})}
+   >
+     Claim NFT!
+   </button>
+ </div>
+
+))
+
 
   if (error) {
     console.error("failed to claim nft", error);
   }
 
-  const mint1 = (
-    <div>
-      {standard}
-      <button
-        disabled={isLoading}
-        onClick={() => claimNft({ to: address, quantity: 1, tokenId: 0 })}
-      >
-        Claim NFT!
-      </button>
-    </div>
-  );
-
-  const mint2 = (
-    <div>
-      {silver}
-      <button
-        disabled={isLoading}
-        onClick={() => claimNft({ to: address, quantity: 1, tokenId: 1 })}
-      >
-        Claim NFT!
-      </button>
-    </div>
-  );
-
-  const mint3 = (
-    <div>
-      {gold}
-      <button
-        disabled={isLoading}
-        onClick={() => claimNft({ to: address, quantity: 1, tokenId: 2 })}
-      >
-        Claim NFT!
-      </button>
-    </div>
-  );
-
-  if (isLoading) {
-    return <div>LOADING</div>;
-  }
-
-  if (error) {
-    console.error("failed to mint nft", error);
-  }
 
   return (
     <div>
+      <h1>NFTBooking Test Drop </h1>
       {address ? (
         <div>
-          <h4>Connected as {address}</h4>
+          <h4>Connected as : <br/> <br/> <br/> {address}</h4>
           <button onClick={disconnectWallet}>Disconnect Metamask Wallet</button>
         </div>
       ) : (
         <button onClick={connectWithMetamask}>Connect Metamask Wallet</button>
       )}
-      {mint1}
-      {mint2}
-      {mint3}
+
+      {nftList}
     </div>
   );
 }
